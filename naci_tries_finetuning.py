e1e376909ec6576686f260cbd0cf3e4ce98ebe6c
horizoneffect <144844508+horizoneffect@users.noreply.github.com>
2025-12-19 02:17:21 -0500

    I made a file for myself

--- naci_tries_finetuning.py
+++ naci_tries_finetuning.py
@@ -1,15 +1,35 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from transformers import Trainer
 from transformers import TrainingArguments
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 import numpy as np
 import evaluate
+import pandas as pd
 
 raw_datasets = load_dataset("imdb")
+print(raw_datasets["train"]["text"])
+print(raw_datasets["train"]["label"])
 metric = evaluate.load("accuracy")
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+def preprocess_data(file_name):
+
+    column_names = ['prompt 1', 'reasoning', 'response 2', 'label']
+    data = pd.read_csv(file_name, header=None, names=column_names, sep='\[\[::\]\]', engine='python', on_bad_lines='skip')
+
+    # game_info is a prefix to remove from 'prompt 1'
+    with open("systemprompt.md", 'r') as f: game_info = f.read()
+    data['prompt 1'] = data['prompt 1'].str.removeprefix(game_info)
+
+    # Convert response 2 to a dict from a string
+    # data['response 2'] = data['response 2'].apply(convert_to_dict)
+
+    dataset = Dataset.from_pandas(data)
+    return dataset
+
+raw_datasets = preprocess_data("finetune.csv")
 
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True)
